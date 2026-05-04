88f3bada5da704933e981e3edb73513ef3eede5e
nolensl0-byte <nolensl0@sewanee.edu>
2026-05-04 01:13:30 -0500

    push

--- data_story_5.r
+++ data_story_5.r
@@ -4,53 +4,64 @@ library(lubridate)
 library(DT)
 
 # Function for saving data to a CSV file
-log_line <- function(newdata, filename = 'app_data.csv'){
-  (dt <- Sys.time() %>% round %>% as.character)
-  (newline <- c(dt, newdata) %>% paste(collapse=',') %>% paste0('\n'))
+log_line <- function(newdata, filename = 'tree_data.csv'){
+  (newline <- c(newdata) %>% paste(collapse=',') %>% paste0('\n'))
   cat(newline, file=filename, append=TRUE)
   print('Data stored!')
 }
 
+
 ################################################################################
 ################################################################################
+
 
 ui <- fluidPage(
-
   titlePanel(h4("Data entry app")),
   br(),
   fluidRow(
-    # Example input: manual text entry
+    column(4,
+           radioButtons(
+              'site_number',
+              label='Site Number',
+              choices = paste('site', 0:6),
+              inline = TRUE,
+              width='40%')),
     
-    # Example input: toggling between options
-    column(4, radioButtons('site_number',
-                           label='Site number',
-                           choices = paste('site', 1:6),
-                           inline = TRUE,
-                           width='100%')),
-  column(4, selectInput( 
-    "select", 
-    "Select options below:", 
-    list("No Arundinaria" = "No Arundinaria", "Arundinaria" = "Arundinaria"),
-    width ='40%'
-  )),
+    column(4,
+           selectInput(
+             'select',
+             'Select options below:',
+             choices = c('No Arundinaria', 'Arundinaria'),
+             width = '40%')
+    ),
+    column(4,
+           textInput('location', 
+                     'Location', 
+                     value = '', 
+                     width = '40%')
+    ),
+    column(4,
+           textInput('tree_id', 
+                     'Tree ID', 
+                     value = '', 
+                     width = '40%')
+    ),
+    column(4,
+           textInput('dbh', 
+                     'DBH', 
+                     value = '', 
+                     width = '40%')
+    ),
+    column(4,
+           textInput('height', 
+                     'Height', 
+                     value = '', 
+                     width = '40%')
+    )
+  ),
   
-  column(4, textInput('location',
-                      label='Location',
-                      value='',
-                      width = '75%')),
-
-    column(4, textInput('tree_id',
-                        label='Tree ID',
-                        value='',
-                        width = '40%')),
-    column(4, textInput('dbh',
-                        label='DBH',
-                        value='',
-                        width = '40%')),
-    column(4, textInput('height',
-                        label='Height',
-                        value='',
-                        width = '40%'))),
+  br(),
+  br(),
   
   fluidRow(column(2),
            # Save button!
@@ -58,48 +69,34 @@ ui <- fluidPage(
                                   h2('Save!'),
                                   width='100%')),
            column(2)),
-  br(), br(), 
-
+  br(),
+  br(),
+ 
   fluidRow(column(12, DTOutput('data')))
-  
-  
-  
 )
-  
 
 
-################################################################################
-################################################################################
-
+# =============================================================================
+# SERVER
+# =============================================================================
 server <- function(input, output) {
   rv <- reactiveValues()
-  rv$df <- read.csv('app_data.csv', header=TRUE)
+  #rv$df <- read.csv('tree_data.csv', header=FALSE)
   
   output$data <- renderDT({
-    df <- read.csv('app_data.csv')
-    rv$df
+    #df <- read.csv('tree_data.csv')
+    #rv$df
   })
-  
-  
-  
   
   # Save button ================================================================
   observeEvent(input$save, {
-    newdata <- c(input$location, input$select, input$site_number,input$tree_id,input$dbh,input$height)
+    newdata <- c(input$site_number, input$select, input$location, input$tree_id, 
+                 input$dbh, input$height)
     log_line(newdata)
     showNotification("Save successful!")
-    rv$df <- read.csv('app_data.csv', header=TRUE)
-    
-
   })
   #=============================================================================
   
-  
-  
 }
-
-
-################################################################################
-################################################################################
-
+#=============================================================================
 shinyApp(ui, server)
